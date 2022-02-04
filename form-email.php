592badbd987042889ca5e12eeb69d2a65789d3aa
em-stea <emiliastea@hotmail.com>
2022-02-04 14:59:43 -0300

    upd

--- form-email.php
+++ form-email.php
@@ -18,7 +18,7 @@ $st->execute($sql_params);
 
 
 /*mail destinatario y contenido a recibir*/
-$email_to = "emilia@telocreo.studio"; 
+$email_to = "hola@alejandraherrera.com.ec"; 
 $contenido = "$nombre ha enviado un mensaje desde la web de alejandra.herrera<br /> Nombre: $nombre<br />Email: $email<br /> Mensaje: $mensaje";
 $asunto = "Consulta desde la Web alejandra.herrera";
 
