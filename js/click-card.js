c7a43eab654120f54fbe6e27e537cda566a6db25
em-stea <emiliastea@hotmail.com>
2022-02-22 12:09:07 -0300

    upd

---
+++ js/click-card.js
@@ -0,0 +1,30 @@
+/*Coffee*/
+//Columna-1
+function mostrarTarjeta() {
+  document.getElementById("frente").style.display = "none";
+  document.getElementById("dorso").style.display = "flex";
+}
+function ocultarTarjeta() {
+  document.getElementById("dorso").style.display = "none";
+  document.getElementById("frente").style.display = "flex";
+}
+
+//Columna-2
+function mostrarTarjetaTwo() {
+  document.getElementById("frente-two").style.display = "none";
+  document.getElementById("dorso-two").style.display = "flex";
+}
+function ocultarTarjetaTwo() {
+  document.getElementById("dorso-two").style.display = "none";
+  document.getElementById("frente-two").style.display = "flex";
+}
+
+/*Hackea tu vida*/
+function mostrarTarjetaThree() {
+  document.getElementById("frente-three").style.display = "none";
+  document.getElementById("dorso-three").style.display = "flex";
+}
+function ocultarTarjetaThree() {
+  document.getElementById("dorso-three").style.display = "none";
+  document.getElementById("frente-three").style.display = "flex";
+}
